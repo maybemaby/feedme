6d3ba0915f61703abb13d8e4cda9fe36d4c9e690
maybemaby <74226064+maybemaby@users.noreply.github.com>
2025-12-02 16:05:24 -0800

    chore: move feed view to remote funcs

--- src/lib/feeds/feed-items.remote.ts
+++ src/lib/feeds/feed-items.remote.ts
@@ -1,8 +1,37 @@
 import { form, getRequestEvent, query } from '$app/server';
 import { error } from '@sveltejs/kit';
 import { z } from 'zod';
-import { countFeedItemsBuilder, feedItemService, findFeedItemsBuilder } from './queries';
+import {
+	countFeedItemsBuilder,
+	feedItemService,
+	findFeedItemsBuilder,
+	findFeedItemsParamsSchema
+} from './queries';
 import { getDb } from '$lib/server/db/db';
+
+const getFeedItemsParamSchema = z.object({
+	page: z.number().int().positive().optional()
+});
+
+export const getFeedItems = query(getFeedItemsParamSchema, async (data) => {
+	const event = getRequestEvent();
+
+	const userId = event.locals.session?.user.id;
+
+	if (!userId) {
+		throw error(401, 'Unauthorized');
+	}
+
+	const [[feedCountRes], feedItemsRes] = await getDb().batch([
+		countFeedItemsBuilder({ userId }),
+		findFeedItemsBuilder({ userId, page: data.page })
+	]);
+
+	return {
+		count: feedCountRes.count,
+		feedItems: feedItemsRes
+	};
+});
 
 const readLaterParamSchema = z.object({
 	itemId: z.number().int().positive()
@@ -23,45 +52,37 @@ export const toggleReadLater = form(readLaterParamSchema, async (data) => {
 		event.locals.logger.error({ error: res.error }, 'Failed to toggle read later');
 		throw error(500, 'Failed to toggle read later');
 	} else {
+		await getFeedItems({ page: 1 }).refresh();
+
 		return {
 			message: 'Toggled read later successfully'
 		};
 	}
 });
 
-const getFeedItemsParamSchema = z.object({
-	page: z.number().int().positive().optional()
-});
+export const findFeedItems = query(findFeedItemsParamsSchema, async (data) => {
+	const { locals } = getRequestEvent();
 
-export const getFeedItems = query(getFeedItemsParamSchema, async (data) => {
-	const event = getRequestEvent();
-
-	const userId = event.locals.session?.user.id;
+	const userId = locals.session?.user.id;
 
 	if (!userId) {
 		throw error(401, 'Unauthorized');
 	}
 
-	const [[feedCountRes], feedItemsRes] = await getDb().batch([
-		countFeedItemsBuilder({ userId }),
-		findFeedItemsBuilder({ userId, page: data.page })
-	]);
-
-	event.locals.logger.debug(
-		{
-			count: feedCountRes.count,
-			res: feedItemsRes.map((r) => ({
-				id: r.id,
-				title: r.title,
-				published: r.publishedAt,
-				readLater: r.readLater
-			}))
-		},
-		'Fetched feed items'
-	);
-
-	return {
-		count: feedCountRes.count,
-		feedItems: feedItemsRes
-	};
+	return await locals.services.feedService.findFeedItems({ ...data, userId });
 });
+
+export const countFeedItems = query(
+	findFeedItemsParamsSchema.omit({ page: true }),
+	async (data) => {
+		const { locals } = getRequestEvent();
+
+		const userId = locals.session?.user.id;
+
+		if (!userId) {
+			throw error(401, 'Unauthorized');
+		}
+
+		return await locals.services.feedService.countFeedItems({ ...data, userId });
+	}
+);
